c185a2fe4e2c8f12f8e1d635e37de613d1237e3e
Nguyen Ninh <nguyenvanninh2004pt@gmail.com>
2025-11-15 03:14:37 +0700

    feat: implement super admin initialization with role and permission setup

--- src/models/associations.ts
+++ src/models/associations.ts
@@ -1,5 +1,6 @@
 import UserModel from './UserModel';
 import RoleModel from './RoleModel';
+import PermissionModel from './PermissionModel';
 
 UserModel.belongsToMany(RoleModel, {
     through: 'user_roles',
@@ -15,7 +16,22 @@ RoleModel.belongsToMany(UserModel, {
     as: 'users',
 });
 
+RoleModel.belongsToMany(PermissionModel, {
+    through: 'role_permissions',
+    foreignKey: 'role_id',
+    otherKey: 'permission_id',
+    as: 'permissions',
+});
+
+PermissionModel.belongsToMany(RoleModel, {
+    through: 'role_permissions',
+    foreignKey: 'permission_id',
+    otherKey: 'role_id',
+    as: 'roles',
+});
+
 export {
     UserModel,
     RoleModel,
+    PermissionModel,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import app from './app';
+import { initSuperAdmin } from './bootstrap/initAdmin';
 import config from './configuration/appConfig';
 import { connectDatabase } from './database/pgClient';
 
 app.listen(config.port, async () => {
     await connectDatabase();
+
+    await initSuperAdmin();
 
     console.log(`Server running on port ${config.port}`);
 });
